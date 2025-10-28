2807d773513b4fc5a215fc9c70fff5672b2c8e79
AhmadAffan28 <ahmad.aff4n@gmail.com>
2025-10-28 14:11:39 +0700

    belajar expendet dan list view

--- lib/about.me.dart
+++ lib/about.me.dart
@@ -3,6 +3,19 @@ import 'package:flutter/material.dart';
 class AboutMe extends StatelessWidget {
   const AboutMe({super.key});
 
+  final List<String> badgeList = const <String>[
+    '😈',
+    '☠️',
+    '🤩',
+    '🧠',
+    '🤬',
+    '🤗',
+    '✌🏻',
+    '😴',
+    '🔥',
+    '🤡',
+    ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,94 +23,380 @@ class AboutMe extends StatelessWidget {
         title: Text('About Me', style: TextStyle(fontWeight: FontWeight.bold),),
         backgroundColor: Colors.amberAccent,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              /**
-               * TEXT NAME
-               */
-              Text(
-                'Apaannn'.toUpperCase(),
-                style: TextStyle(
-                  fontSize: 18, 
-                  fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                /**
+                 * IMAGE PROFILE
+                 */
+              //   Image.asset('assets/profile.png',
+              //   width: 100,
+              //   height: 100,
+              // ),
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: NetworkImage(
+                  'assets/IMG_7777.JPG',
                 ),
               ),
-              /**
-               * SPACER
-               */
-              SizedBox(
-                height: 10,
-              ),
-              /**
-               * TEXT ABOUT
-               */
-              Text(
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-                style: TextStyle(),
-                textAlign: TextAlign.justify,
-              ),
-              /**
-               * SPACER
-               */
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.amberAccent,
-                  borderRadius: BorderRadius.circular(15),
-                  border: Border.all(
-                    color: Colors.black12,
-                    width: 2,
-                  ),
-                  boxShadow: [
-                    BoxShadow(
-                      blurRadius: 2,
-                      offset: Offset(2, 3),
-                      color: Colors.black,
-                    )
-                  ],
-                ),
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      width: 75,
-                      height: 75,
-                      decoration: BoxDecoration(
-                        color: Colors.greenAccent,
-                        borderRadius: BorderRadius.circular(
-                          15
-                        ),
-                      ),
-                      child: Icon(
-                        Icons.android,
-                        color: Colors.white,
-                        size: 50,
-                      ),
+                /**
+                 * TEXT NAME
+                 */
+                Padding(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  child: Text(
+                    'Ahmad Affan'.toUpperCase(),
+                    style: TextStyle(
+                      fontSize: 18, 
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Chunq'
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Android Project\'s',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
+                  ),
+                ),
+                /**
+                 * SPACER
+                 */
+                SizedBox(
+                  height: 10,
+                ),
+                /**
+                 * TEXT ABOUT
+                 */
+                Text(
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                  style: TextStyle(),
+                  textAlign: TextAlign.justify,
+                ),
+                /**
+                 * SPACER
+                 */
+                SizedBox(
+                  height: 15,
+                ),
+                /**
+                 * CARD CONTAINER ANDROID
+                 */
+                Row(
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.amber,
+                            borderRadius: BorderRadius.circular(
+                              20
                             ),
                           ),
-                          Text('10 APK'),
-                        ],
+                          child: Row(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(4.0),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.greenAccent,
+                                    borderRadius: BorderRadius.circular(
+                                      20,
+                                    ),
+                                  ),
+                                  child: Icon(
+                                    Icons.gamepad,
+                                    size: 75,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'GAME PROJECT',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                    Text(
+                                      '10 Gaes',
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.amber,
+                            borderRadius: BorderRadius.circular(15),
+                            border: Border.all(
+                              color: Colors.black12,
+                              width: 2,
+                            ),
+                            boxShadow: [
+                              BoxShadow(
+                                blurRadius: 2,
+                                offset: Offset(2, 3),
+                                color: Colors.black,
+                              )
+                            ],
+                          ),
+                          child: Row(
+                            children: <Widget>[
+                              Container(
+                                width: 75,
+                                height: 75,
+                                decoration: BoxDecoration(
+                                  color: Colors.greenAccent,
+                                  borderRadius: BorderRadius.circular(
+                                    15
+                                  ),
+                                ),
+                                child: Icon(
+                                  Icons.android,
+                                  color: Colors.white,
+                                  size: 50,
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Android Project\'s',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Text('10 APK'),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
                     ),
                   ],
                 ),
-              ),
-              Text('Experience')
-            ],
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 10,
+                  ),
+                  child: Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(
+                        20
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text('Schedule'.toUpperCase(),
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                        letterSpacing: 2,
+                        color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                //  ROW SCHEDULE
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Expanded(
+                      flex: 1,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          // width: double.infinity ngasih lebar full
+                          decoration: BoxDecoration(
+                            color: Colors.green,
+                            borderRadius: BorderRadius.circular(
+                              20,
+                            ),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              children: <Widget>[
+                                Text('Belajar', style: TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                                Icon(
+                                Icons.timer,
+                                size: 30,
+                                ),
+                                Text(
+                                  '07.30 - 14.30',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    /**
+                     * MEMBACA
+                     */
+                    Expanded(
+                      flex: 1,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 255, 217, 0),
+                            borderRadius: BorderRadius.circular(
+                              20,
+                            ),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              children: <Widget>[
+                                Text(
+                                  'MEMBACA',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                                Icon(
+                                  Icons.book,
+                                  size: 30,
+                                ),
+                                Text(
+                                  '20.00 - 21.00',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.greenAccent,
+                            borderRadius: BorderRadius.circular(
+                              20,
+                            ),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              children: <Widget>[
+                                Text(
+                                  'TIDUR',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                                Icon(
+                                  Icons.bed,
+                                  size: 30,
+                                ),
+                                Text(
+                                  '21.30 - 03.15',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.cyanAccent,
+                    borderRadius: BorderRadius.circular(
+                      20
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: Text(
+                      'BADGES', 
+                      style: TextStyle(
+                        fontFamily: 'Chunq',
+                        letterSpacing: 5,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 110,
+                  child: ListView(
+                    /**
+                     * badgeList = Variable list diatas
+                     * element = nama variable dari map
+                     * return = tempat widget dari data list
+                     */
+                    scrollDirection: Axis.horizontal,
+                    children: badgeList.map((e){
+                      return Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Container(
+                          width: 100,
+                          height: 100,
+                          decoration: BoxDecoration(
+                            color: Colors.redAccent,
+                            borderRadius: BorderRadius.circular(
+                              20,
+                              ),
+                          ),
+                          child: Center(
+                            child: Text(
+                              '${e}',
+                              style: TextStyle(
+                                fontSize: 60,
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
